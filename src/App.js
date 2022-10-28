5c3e94fae3c0677adce1b97affdf5bd59c2825b8
Victor Matias <fvictor1705@gmail.com>
2022-10-28 01:14:46 -0300

    corrigi rotas

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 import './css/App.css';
 
+const rota = 'https://victormatia.github.io/wallet-redux/';
+
 export default class App extends Component {
   render() {
     return (
       <main>
         <Switch>
-          <Route exact path="/wallet-redux/" component={ Login } />
+          <Route exact path="rota" component={ Login } />
           <Route exact path="/carteira" component={ Wallet } />
         </Switch>
       </main>
